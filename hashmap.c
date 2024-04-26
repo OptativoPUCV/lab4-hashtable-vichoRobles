5ebe55610222bf23ac9754abaf784e5bc5d39bf8
vichoRobles <163026879+vichoRobles@users.noreply.github.com>
2024-04-26 07:59:12 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -158,7 +158,19 @@ Pair * firstMap(HashMap * map)
   return NULL;
 }
 
-Pair * nextMap(HashMap * map) {
+Pair * nextMap(HashMap * map) 
+{
+  if (map == NULL) return NULL;
+  if (map->current == -1) return NULL;
+  int i;
+  for (i = map->current + 1; i < map->capacity; i++)
+    {
+      if (map->buckets[i] != NULL)
+      {
+        map->current = i;
+        return map->buckets[i];
+      }
+    }
 
     return NULL;
 }
